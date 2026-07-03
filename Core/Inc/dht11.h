0600bef14c0709a2671f589ff41783f103b2c6fe
suanlayu666 <2742263464@qq.com>
2026-07-03 19:10:04 +0800

    init

---
+++ Core/Inc/dht11.h
@@ -0,0 +1,19 @@
+#ifndef __DHT11_H
+#define __DHT11_H 
+#include "main.h"   
+ 
+//IO方向设置
+#define DHT11_HIGH     HAL_GPIO_WritePin(GPIOA, DHT11_Pin,	GPIO_PIN_SET)
+#define DHT11_LOW      HAL_GPIO_WritePin(GPIOA, DHT11_Pin, GPIO_PIN_RESET)
+#define DHT11_DQ_IN    HAL_GPIO_ReadPin(GPIOA, DHT11_Pin)
+#define u8 uint8_t
+ 
+u8 DHT11_Init(void);//初始化DHT11
+u8 DHT11_Read_Data(u8 *temp,u8 *humi);//读取温湿度
+u8 DHT11_Read_Byte(void);	//读出一个字节
+u8 DHT11_Read_Bit(void);	//读出一个位
+u8 DHT11_Check(void);		//检测是否存在DHT11
+void DHT11_Rst(void);		//复位DHT11    
+#endif
+ 
+ 
